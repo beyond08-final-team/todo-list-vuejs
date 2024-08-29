e5de506c4f26464bfd7ee416e90254a1813884b2
yongun2 <djkye0001@naver.com>
2024-08-30 08:03:14 +0900

    feat: 전체적인 퍼블리싱 및 더미 데이터 추가

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,86 @@
 <template>
-  <b-button>hello</b-button>
+  <todo-layout>
+    <b-card class="todo-card shadow">
+      <b-card-title class="text-center">To do App</b-card-title>
+      <b-card-body
+        class="todo-input-group w-75 d-flex flex-nowrap justify-content-around m-auto"
+      >
+        <b-form-input class="w-50" />
+        <b-button variant="primary">SAVE</b-button>
+        <b-button variant="warning">GET TODOS</b-button>
+      </b-card-body>
+      <b-card-body class="d-flex flex-nowrap justify-content-around">
+        <b-table-simple>
+          <b-thead>
+            <b-th> No. </b-th>
+            <b-th> Todo Item </b-th>
+            <b-th> Status </b-th>
+            <b-th> Actions </b-th>
+          </b-thead>
+
+          <b-tbody>
+            <b-tr v-for="todo in todos" :key="todo.id">
+              <b-td>{{ todo.no }}</b-td>
+              <b-td>{{ todo.content }}</b-td>
+              <b-td>{{ todo.status }}</b-td>
+              <b-td class="d-none d-sm-block">
+                <b-button class="me-3" variant="danger">DELETE</b-button>
+                <b-button class="me-3" variant="dark">EDIT</b-button>
+                <b-button variant="success">FINISHED</b-button>
+              </b-td>
+              <b-td class="d-sm-none">
+                <b-dropdown variant="outline-secondary">
+                  <b-dropdown-item> DELETE </b-dropdown-item>
+                  <b-dropdown-divider />
+                  <b-dropdown-item> FINISHED </b-dropdown-item>
+                  <b-dropdown-divider />
+                  <b-dropdown-item> EDIT </b-dropdown-item>
+                </b-dropdown>
+              </b-td>
+            </b-tr>
+          </b-tbody>
+        </b-table-simple>
+      </b-card-body>
+    </b-card>
+  </todo-layout>
 </template>
 
 <script>
-export default {};
+import { TodoLayout } from "./views";
+
+export default {
+  components: {
+    TodoLayout,
+  },
+  data() {
+    return {
+      todos: [
+        {
+          no: 1,
+          content: "hello",
+          status: "In Progress",
+        },
+        {
+          no: 2,
+          content: "hello",
+          status: "In Progress",
+        },
+        {
+          no: 3,
+          content: "hello",
+          status: "In Progress",
+        },
+      ],
+    };
+  },
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.todo-card {
+  height: 420px;
+}
+.todo-input-group {
+  min-width: 450px;
+}
+</style>
