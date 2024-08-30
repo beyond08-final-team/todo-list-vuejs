af7ace82b7cf513737f49acb3c9d4b241662fa19
hoyeon96 <hoyen96@naver.com>
2024-08-30 16:41:57 +0900

    design: TodoApp 내 요소 간 margin 추가

--- src/components/TodoApp.vue
+++ src/components/TodoApp.vue
@@ -1,38 +1,36 @@
 <template>
-    <main>
-        <div class="TodoBox">
-            <BCardTitle class="text-center">To Do App</BCardTitle>
-            <BCardBody class="d-flex w-75 justify-content-around m-auto">
-                <BFormInput class="w-50" v-model="newTask" placeholder="Enter a task Here"/>
-                <BButton variant="primary" @click="saveTask">SAVE</BButton>
-                <BButton variant="warning">GET TASKS</BButton>
-            </BCardBody>
-            <BCardBody class="d-flex justify-content-around">
-                <BTableSimple>
-                    <BThead head-variant="dark">
-                        <BTr>
-                            <BTh>No.</BTh>
-                            <BTh>Todo item</BTh>
-                            <BTh>Status</BTh>
-                            <BTh>Actions</BTh>
-                        </BTr>
-                    </BThead>
-                    <BTbody>
-                        <BTr v-for="(todo, idx) in todos" :key="todo.index">
-                            <BTd>{{ todo.index }}</BTd>
-                            <BTd>{{ todo.content }}</BTd>
-                            <BTd>{{ todo.status }}</BTd>
-                            <BTd>
-                                <b-button class="me-2" variant="danger" @click="deleteTask(idx)">DELETE</b-button>
-                                <b-button class="me-2" variant="success" @click="finishTask(idx)">FINISHED</b-button>
-                                <b-button class="me-2" variant="dark" @click="editTask">EDIT</b-button>
-                            </BTd>
-                        </BTr>
-                    </BTbody>
-                </BTableSimple>
-            </BCardBody>
-        </div>
-    </main>
+    <div class="todoBox shadow-sm p-4 mb-5 bg-white rounded">
+        <BCardTitle class="text-center mb-4">To Do App</BCardTitle>
+        <BCardBody class="d-flex w-75 justify-content-around m-auto mb-4">
+            <BFormInput class="w-50 me-2" v-model="newTask" placeholder="Enter a task Here"/>
+            <BButton variant="primary" @click="saveTask">SAVE</BButton>
+            <BButton variant="warning">GET TASKS</BButton>
+        </BCardBody>
+        <BCardBody class="d-flex justify-content-around">
+            <BTableSimple class="mt-4">
+                <BThead head-variant="dark">
+                    <BTr>
+                        <BTh>No.</BTh>
+                        <BTh>Todo item</BTh>
+                        <BTh>Status</BTh>
+                        <BTh>Actions</BTh>
+                    </BTr>
+                </BThead>
+                <BTbody>
+                    <BTr v-for="(todo, idx) in todos" :key="todo.index">
+                        <BTd>{{ todo.index }}</BTd>
+                        <BTd>{{ todo.content }}</BTd>
+                        <BTd>{{ todo.status }}</BTd>
+                        <BTd>
+                            <b-button class="me-2" variant="danger" @click="deleteTask(idx)">DELETE</b-button>
+                            <b-button class="me-2" variant="success" @click="finishTask(idx)">FINISHED</b-button>
+                            <b-button class="me-2" variant="dark" @click="editTask">EDIT</b-button>
+                        </BTd>
+                    </BTr>
+                </BTbody>
+            </BTableSimple>
+        </BCardBody>
+    </div>
 </template>
 
 <script>
@@ -90,10 +88,10 @@ export default {
 </script>
 
 <style scoped>
-.TodoBox{
-  background-color: white; /* 박스의 배경색 */
-  border: 1px solid #ccc;  /* 박스의 테두리 */
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); /* 박스 그림자 (옵션) */
-
+.todoBox{
+    min-width: 720px;
+    background-color: white; /* 박스의 배경색 */
+    border: 1px solid #ccc;  /* 박스의 테두리 */
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); /* 박스 그림자 (옵션) */
 }
 </style>
