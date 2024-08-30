a7ab264c45f26da12119d37f26b8ec416fa285ea
hoyeon96 <hoyen96@naver.com>
2024-08-30 16:26:15 +0900

    refactor: 미사용 style구문 삭제

--- src/components/TodoApp.vue
+++ src/components/TodoApp.vue
@@ -94,9 +94,6 @@ export default {
   background-color: white; /* 박스의 배경색 */
   border: 1px solid #ccc;  /* 박스의 테두리 */
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); /* 박스 그림자 (옵션) */
-.buttonTextWhite {
-    color: white;
-}
 
 }
 </style>
