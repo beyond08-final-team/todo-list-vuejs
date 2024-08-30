24c48dc656b7185a0af259cc6d949bf96e82d3ef
yongun2 <djkye0001@naver.com>
2024-08-30 15:37:18 +0900

    chore: pinia, 일반 버전 컴포넌트 등록

---
+++ src/components/index.js
@@ -0,0 +1,16 @@
+import TodoInputForm from "./Form/TodoInputForm.vue";
+import TodoList from "./Todo/TodoList.vue";
+import TodoListItem from "./Todo/TodoListItem.vue";
+
+import PiniaTodoInputForm from "./Form/PiniaTodoInputForm.vue";
+import PiniaTodoList from "./Todo/PiniaTodoList.vue";
+import PiniaTodoListItem from "./Todo/PiniaTodoListItem.vue";
+
+export {
+  TodoInputForm,
+  TodoList,
+  TodoListItem,
+  PiniaTodoInputForm,
+  PiniaTodoList,
+  PiniaTodoListItem,
+};
