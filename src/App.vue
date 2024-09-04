af2281dbec3cf46cb817ac09e62e7efc14528e75
hoyeon96 <hoyen96@naver.com>
2024-09-04 14:58:40 +0900

    Revert Merge Conflict

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,5 @@
 <template>
-<<<<<<< HEAD
-  <div class="d-flex justify-content-center align-items-center vh-100 background">
-    <TodoApp />
-  </div>
-=======
   <b-button>hello</b-button>
->>>>>>> parent of 366b7e3 (design: 바탕 배경화면 색 변경)
 </template>
 
 <script>
